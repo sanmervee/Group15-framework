ea201cdae26a0bdb18afbe9be00fcbed322696db
SueTheCoder <suheyla315@hotmail.com>
2021-07-14 16:42:23 -0400

    IQ - swapping number

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -6,6 +6,20 @@ public class Hello {
         System.out.println("wooden spoon");
         System.out.println("Hi from Kady");
         System.out.println("hello from suheyla");
-
+        swapNum(10,20);
     }
+
+    /**
+     * Interview Question - Swapping number with method
+     * @param a
+     * @param b
+     */
+    public static void swapNum(int a, int b){
+        a = a - b;   //a=-10
+        b = a + b;   // b=10
+        a = b - a; // a=20
+        System.out.print("a = " + a+ "b = " + b);
+    }
+
+
 }
