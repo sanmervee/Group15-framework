9f117925f98aded71ccd939814ac72d0827b28fb
Christina Gorbachev <cristinochka85@gmail.com>
2021-07-14 20:57:06 +0300

    added more code

--- src/test/java/HelloFromMyBranch.java
+++ src/test/java/HelloFromMyBranch.java
@@ -2,5 +2,6 @@ public class HelloFromMyBranch {
     public static void main(String[] args) {
 
         System.out.println("Hello from my branch");
+        System.out.println("Adding more info");
     }
 }
