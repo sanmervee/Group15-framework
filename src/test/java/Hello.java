8446f91bb914c5f91817a61fe13868ae9dc658ef
Raushaniya <canym83@gmail.com>
2021-07-14 14:14:39 -0400

    some more work

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Hello from Rau!");
+        System.out.println("let's do it!");
 
     }
 }
